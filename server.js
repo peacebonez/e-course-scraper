1c09a2f6c2e177331bc7be78c8a76a2cbf10e84b
peacebonez <kmpariso12@gmail.com>
2020-09-12 15:36:41 -0400

    got front end runnig on port 5000

--- server.js
+++ server.js
@@ -4,20 +4,36 @@ const scraper = require("./course-scrape");
 const app = express();
 
 const PORT = 5000;
+app.listen(PORT || 5000);
+
+console.log("Server up and running at " + PORT);
 
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "client", "public")));
 
+//home page
+app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+// home page
+// app.get("/", (req, res) => {
+//   try {
+//     console.log("__dirname:", __dirname);
+//     res.sendFile("/client/build/index.html", { root: __dirname });
+//     // res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+//     // res.sendFile(__dirname + "/client/public/index.html");
+//     // res.sendFile(__dirname + "/client/public/sword.jpg");
+//   } catch (err) {
+//     return console.error(err);
+//   }
+// });
+
+//search query
 app.get("/search", async (req, res) => {
-  //parameter req.params.query
-  const course = req.query;
-  //   res.send(course.course);
-  const courses = await scraper(course.course);
+  const query = req.query;
+  console.log("query.course:", query.course);
+  const courses = await scraper(query.course);
 
   res.json(courses);
 });
 
-app.listen(PORT || 3000);
-
-console.log("Server up and running at " + PORT);
+module.exports = app;
