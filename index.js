c37bada1bf0c7465c978bdc55c031cb14b9f09f8
peacebonez <kmpariso12@gmail.com>
2020-09-11 23:06:53 -0400

    adding Udemy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 const query = "javascript";
 
 (async () => {
+  let courses = [];
   const COURSERA = "https://www.coursera.org/search";
   const UDEMY = "https://www.udemy.com/";
   const EDX = "https://www.edx.org/";
@@ -12,8 +13,68 @@ const query = "javascript";
   const page = await browser.newPage();
 
   //coursera
+  // try {
+  //   await page.goto(COURSERA, { waitUntil: "load" });
+
+  //   //wait for input elements to load
+  //   await page.waitForSelector("input");
+  // } catch (err) {
+  //   console.error(err);
+  //   await browser.close();
+  // }
+
+  // const courseraInput = (await page.$$(".react-autosuggest__input"))[1];
+  // await courseraInput.type(query);
+  // await courseraInput.type(String.fromCharCode(13));
+
+  // await page.waitFor(800);
+  // const courseraList = await page.$$(".ais-InfiniteHits-item");
+  // //   console.log(courseraList);
+
+  // let courseraCourses = [];
+  // for (let item of courseraList) {
+  //   const link = await item
+  //     .$eval("a", (a) => a.href)
+  //     .catch((err) => console.log("no link"));
+
+  //   // console.log(link);
+
+  //   const courseName = await item
+  //     .$eval("h2", (name) => name.textContent)
+  //     .catch((err) => console.log("no course name"));
+
+  //   // console.log(courseName);
+
+  //   const partnerName = await item
+  //     .$eval(".partner-name", (name) => name.textContent)
+  //     .catch((err) => console.log("no partner name"));
+
+  //   // console.log(partnerName);
+
+  //   const image = await item
+  //     .$eval(".image-wrapper img", (img) => img.src)
+  //     .catch((err) => console.log("no image"));
+
+  //   // console.log(image);
+
+  //   const courseraItem = {
+  //     courseName,
+  //     partnerName,
+  //     image,
+  //     link,
+  //   };
+
+  //   courseraCourses = [...courseraCourses, courseraItem];
+
+  //   //courses = [...courseraCourses, udemyCourses..., edxCourses..., mitCourses...]
+  // }
+
+  // console.log(courseraCourses);
+
+  //udemy
+
   try {
-    await page.goto(COURSERA, { waitUntil: "load" });
+    await page.goto(UDEMY, { waitUntil: "load" });
 
     //wait for input elements to load
     await page.waitForSelector("input");
@@ -22,59 +83,16 @@ const query = "javascript";
     await browser.close();
   }
 
-  const courseraInput = (await page.$$(".react-autosuggest__input"))[1];
-  await courseraInput.type(query);
-  await courseraInput.type(String.fromCharCode(13));
+  const udemyInput = await page.$(".billboard--billboard--3-fQr input");
+
+  await udemyInput.type(query);
+  await udemyInput.type(String.fromCharCode(13));
 
   await page.waitFor(800);
-  const courseraList = await page.$$(".ais-InfiniteHits-item");
-  //   console.log(courseraList);
-
-  let courseraCourses = [];
-  for (let item of courseraList) {
-    const link = await item
-      .$eval("a", (a) => a.href)
-      .catch((err) => console.log("no link"));
-
-    // console.log(link);
-
-    const courseName = await item
-      .$eval("h2", (name) => name.textContent)
-      .catch((err) => console.log("no course name"));
-
-    // console.log(courseName);
-
-    const partnerName = await item
-      .$eval(".partner-name", (name) => name.textContent)
-      .catch((err) => console.log("no partner name"));
-
-    // console.log(partnerName);
-
-    const image = await item
-      .$eval(".image-wrapper img", (img) => img.src)
-      .catch((err) => console.log("no image"));
-
-    // console.log(image);
-
-    const courseraItem = {
-      courseName,
-      partnerName,
-      image,
-      link,
-    };
-
-    courseraCourses = [...courseraCourses, courseraItem];
-  }
-
-  console.log(courseraCourses);
-
-  return;
-
-  //udemy
 
   //EDX
 
   //MIT
 
-  await browser.close();
+  // await browser.close();
 })();
