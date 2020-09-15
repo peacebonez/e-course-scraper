81a16015ea3d4e0b9626f371b51ed117d71eb413
peacebonez <kmpariso12@gmail.com>
2020-09-14 21:48:34 -0400

    search bar calls scraper function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Header from "./components/Header";
 import Landing from "./components/Landing";
@@ -9,16 +11,18 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/search" component={CourseContainer} />
-        </Switch>
-      </Router>
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/search" component={CourseContainer} />
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
+    </Provider>
   );
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+
+import { connect } from "react-redux";
+import { getCourses } from "../actions/courses-action";
+
 import logo from "../assets/stem-icon.png";
 import { FaSearch } from "react-icons/fa";
 import { Link, useHistory } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ getCourses, courses }) => {
   const [userQuery, setUserQuery] = useState("");
 
   const history = useHistory();
@@ -13,6 +17,8 @@ const Header = () => {
     if (!userQuery) return;
     else {
       history.push(`/search?course=${userQuery}`);
+      console.log("userQuery:", userQuery);
+      getCourses(userQuery);
       setUserQuery("");
     }
   };
@@ -48,4 +54,8 @@ const Header = () => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  courses: state.courses.courses,
+});
+
+export default connect(mapStateToProps, { getCourses })(Header);
