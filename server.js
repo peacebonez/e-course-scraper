c22a4960fcd56af73566618338c0107605b4c0f6
peacebonez <kmpariso12@gmail.com>
2020-09-22 12:47:10 -0400

    changed static folder in production mode

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ app.get("/search", async (req, res) => {
   res.json(courses);
 });
 
+if (process.env.NODE_ENV === "production") {
+  //Set the static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || PORT);
 
 module.exports = app;
